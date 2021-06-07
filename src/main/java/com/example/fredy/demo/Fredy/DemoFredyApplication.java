92fdcf3c176407bbea6f95c8e28e9005a718ec05
Yaher <yaher.carrillo@teloggy.com>
2021-06-07 10:13:38 -0500

    Req - Dev : Agregando mi desarrollo

---
+++ src/main/java/com/example/fredy/demo/Fredy/DemoFredyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fredy.demo.Fredy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFredyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFredyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fredy/demo/Fredy/DemoFredyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fredy.demo.Fredy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFredyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
